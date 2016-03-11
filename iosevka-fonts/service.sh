5799b022e2a352baf0d9c0bf127b10d88f3433ac
Maciek Krawiec <maciek.krawiec@zoho.com>
2016-03-11 21:30:43 +0100

    Bump iosevka-fonts to 1.8.2

--- iosevka-fonts/service.sh
+++ iosevka-fonts/service.sh
@@ -1,2 +1,2 @@
-PKG_VERSION=1.7.5
-PKG_DOWNLOAD_URL=https://github.com/be5invis/Iosevka/releases/download/v${PKG_VERSION}/iosevka-${PKG_VERSION}.zip
+PKG_VERSION=1.8.2
+PKG_DOWNLOAD_URL=https://github.com/be5invis/Iosevka/releases/download/v${PKG_VERSION}/a-iosevka-${PKG_VERSION}.zip
